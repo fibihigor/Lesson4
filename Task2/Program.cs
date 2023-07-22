a63fcc6e7211026b65cf805ca1ee267bbd8c9155
igor <fibih.igor94@yandex.ru>
2023-07-22 15:48:00 +0300

    Task №2

---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Пример:
+452 -> 11
+82 -> 10
+9012 -> 12
+
+*/
+
+Console.Write("Введите число: ");
+string numbers = Console.ReadLine();
+
+int SumNumbersInNumber(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+    return sum;
+}
+
+void ExceptionHandling(string enterA)
+{
+    bool yesInt = int.TryParse(enterA, out int result);
+
+    if (yesInt && result >= 0) // Непонятно, что делать с отрицательными числами и 0 =(
+    {
+        int sumResult = SumNumbersInNumber(result);
+        Console.WriteLine("Сумма цифр числа {0} = {1}",
+                          result, sumResult);
+    }
+    else
+    {
+        Console.WriteLine("{0} -> Некорректный ввод!", numbers);
+    }
+}
+
+ExceptionHandling(numbers);
